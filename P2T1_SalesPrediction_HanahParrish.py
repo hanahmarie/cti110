ea39a3f24c673107fe970db68d36da2beb76c22d
hanahmarie <55773843+hanahmarie@users.noreply.github.com>
2019-09-24 22:25:14 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_HanahParrish.py
@@ -0,0 +1,13 @@
+# This is the Sales Prediction Program
+# 09/22/2019
+# CTI-110-P2T1
+# Hanah Parrish
+#
+# Get the projected total sales.
+total_sales = float (input('Enter the projected saled:'))
+
+# Calculate the profit as 23 perccent of the sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $', format(profit, ',.2f')) 
